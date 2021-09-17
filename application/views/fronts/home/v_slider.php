53becdec98e5125fc31f674a2f337609371cd949
arkans <82564511+arkansys22@users.noreply.github.com>
2021-09-17 11:48:44 +0700

    up

--- application/views/fronts/home/v_slider.php
+++ application/views/fronts/home/v_slider.php
@@ -6,17 +6,15 @@
           if(empty($post_new->slider_gambar)) {
             echo "<div class='carousel-item background_bg ' data-img-src='".base_url()."assets/images/blog_small_img1_350X198.jpg'>";
             }else {
-            echo " <div class='carousel-item ".$post_new->slider_meta_desk." background_bg ' data-img-src='".base_url()."assets/frontend/slider/".$post_new->slider_gambar."'> ";}
+            echo " <div class='carousel-item ".$post_new->slider_meta_desk." background_bg' data-img-src='".base_url()."assets/frontend/slider/".$post_new->slider_gambar."'> ";}
          ?>
             <div class="banner_slide_content">
                 <div class="container"><!-- STRART CONTAINER -->
                     <div class="row justify-content-center">
                         <div class="col-lg-9 col-sm-12 text-center">
                             <div class="banner_content animation text_white" data-animation="fadeIn" data-animation-delay="0.8s">
-                                <p style="font-size: 45px; font-style: Montserrat;"class="animation text-uppercase" data-animation="fadeInDown" data-animation-delay="1s"><b style="color:#000"><?php echo $post_new->slider_judul ?></b></p>
-                                  <a style="font-size: 12px; "class="btn btn-default btn-radius btn-sm mb-2 animation animated fadeInUp" href="<?php echo $post_new->slider_link ?>" data-animation="fadeInUp" data-animation-delay="1.8s" style="animation-delay: 1.8s; opacity: 1;"><i style="font-size:12px;color: #000; font-style: Montserrat;">Selengkapnya<i></a>
-                                 
-
+                                <h2 style="font-size: 60px; "class="animation text-uppercase" data-animation="fadeInDown" data-animation-delay="1s"><?php echo $post_new->slider_judul ?></h2>
+                                  <a class="btn btn-default btn-radius btn-sm mb-2 animation animated fadeInUp" href="<?php echo $post_new->slider_keyword ?>" data-animation="fadeInUp" data-animation-delay="1.8s" style="animation-delay: 1.8s; opacity: 1;">Selengkapnya</a>
 
                                 <!-- <p style="font-size: 16px;" class="animation my-4" data-animation="fadeInUp" data-animation-delay="1.5s"><?php echo $post_new->slider_desk ?></p> -->
 
