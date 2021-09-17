de13e4a3f16b9027cd1599318a5ab5b03ad02d9f
sansD92 <adjiedwisandy@gmail.com>
2021-09-18 00:43:24 +0700

    up

--- application/controllers/Produk.php
+++ application/controllers/Produk.php
@@ -17,6 +17,7 @@ public function index()
       $data['identitas']= $this->Crud_m->get_by_id_identitas($id='1');
       $data['posts_bisnis'] = $this->Crud_m->view_where_orders('bisnis_kategori','bisnis_kategori_status','bisnis_kategori_id','ASC');
        $data['posts_produk'] = $this->Crud_m->get_all_produk();
+        $data['produks'] = $this->db->query("select * from produk   ORDER BY  urutan_produk ")->result();
 
 
 
