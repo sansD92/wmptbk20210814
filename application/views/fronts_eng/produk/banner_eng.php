f84926638bd854e0f51dc4a1dba8a91e8f32943f
arkans <82564511+arkansys22@users.noreply.github.com>
2021-09-28 13:51:57 +0700

    up

--- application/views/fronts_eng/produk/banner_eng.php
+++
@@ -1,8 +0,0 @@
-<!-- START SECTION BANNER -->
-<section class="background_bg breadcrumb_section overlay_bg2 page-title-light" style="height:500px;"data-img-src="<?php echo base_url()?>assets/frontend/campur/Sub-Produk.png">
-	<div class="container">
-    	<div class="row">
-        	
-    </div>
-</section>
-<!-- END SECTION BANNER -->
